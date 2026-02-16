2e116ab5081d4ec12a2933539d1cc5d8e2d3e8b1
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-02-16 08:57:53 +0500

    update

--- server/blog/models.py
+++ server/blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from shared.models import BaseModel
+from users.models import User
+from django.core.validators import FileExtensionValidator
+
+
+class Blog(BaseModel):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blogs")
+    title = models.CharField(max_length=250)
+    text = models.TextField()
+    image = models.ImageField(
+        upload_to="blog",
+        validators=[
+            FileExtensionValidator(
+                allowed_extensions=["jpg", "jpeg", "png", "heic", "heif"]
+            ),
+        ],
+    )
+
+    def __str__(self):
+        return f"{self.title}"
+
 
 # Create your models here.
--- server/youcars/settings.py
+++ server/youcars/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     # apps
     "users",
     "cars",
+    "blog",
 ]
 
 MIDDLEWARE = [
