7f2a36e07158bf148cf60989bfa5f5147450dae2
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-02-16 12:00:44 +0500

    update

--- server/blog/urls.py
+++ server/blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import BlogPostView
+from .views import BlogPostView , GetBlogsView
 
 urlpatterns = [
     path('blog/' , BlogPostView.as_view()),
+    path('blog/all/' , GetBlogsView.as_view()),
 ] 
 
--- server/blog/views.py
+++ server/blog/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from rest_framework.generics import ListAPIView, CreateAPIView, ListCreateAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView, ListCreateAPIView , RetrieveAPIView
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Blog
 from .serializers import BlogSerializer
 from .permissions import BlogCreatePermissions
+from shared.custom_pagination import CustomPagination
 
 
 class BlogPostView(CreateAPIView):
@@ -13,6 +14,18 @@ class BlogPostView(CreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+
+class GetBlogsView(ListAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = BlogSerializer
+    queryset = Blog.objects.all().order_by("-created_time")
+    pagination_class = CustomPagination
+    
+class DetailBlogView(RetrieveAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = BlogSerializer
+    pass
 
 
 # Create your views here.
